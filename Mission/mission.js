1ba439db4f2f9b032238cfe9e4c2396eb85dbe6d
Elisontejadab <elison.tejada.b@gmail.com>
2024-09-28 13:28:48 -0600

    upda

--- Mission/mission.js
+++ Mission/mission.js
@@ -1,18 +1,18 @@
-// Test my code
-const themeSelector = document.getElementById("theme-selector");
-    const logo = document.getElementById("logo");
-    
-    themeSelector.addEventListener("change", changeTheme);
-    
-    function changeTheme() {
-        const selectedTheme = themeSelector.value;
-    
-        if (selectedTheme === "dark") {
-            document.body.classList.add("dark");
-            logo.src = "./images/logo_dark.png";  // Ensure correct image path
-        } else {
-            document.body.classList.remove("dark");
-            logo.src = "./images/blue-logo.png";   // Ensure correct image path
-        }
-    }
-    
+
+   const themeSelector = document.getElementById("theme-selector");
+   const logo = document.getElementById("logo");
+
+   themeSelector.addEventListener("change", changeTheme);
+
+   function changeTheme() {
+       const selectedTheme = themeSelector.value;
+
+       if (selectedTheme === "dark") {
+           document.body.classList.add("dark");
+           logo.src = "./images/logo_dark.png";  // Ensure correct image path
+       } else {
+           document.body.classList.remove("dark");
+           logo.src = "./images/blue-logo.png";   // Ensure correct image path
+       }
+   } 
+   
